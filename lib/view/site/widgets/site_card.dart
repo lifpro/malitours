23c19b8c59c0ad2c8f1f226d9976577d1293452d
«Lif» <ysalissou@gmail.com>
2024-01-23 19:56:13 +0000

    Checkbox

--- lib/view/site/widgets/site_card.dart
+++ lib/view/site/widgets/site_card.dart
@@ -18,12 +18,30 @@ class SiteCard extends StatelessWidget {
         ),
       ),
       title: Text(site.nom),
-      subtitle: Text(site.adresse),
-      trailing: Column(
+      subtitle: Row(
         children: [
-          const Text('Tarif'),
-          Text(site.tarif.toString()),
+          RichText(
+              text: TextSpan(
+                  style: DefaultTextStyle.of(context).style,
+                  children: <TextSpan>[
+                TextSpan(
+                    text: site.adresse,
+                    style: const TextStyle(color: Colors.black)),
+                const TextSpan(
+                    text: ',', style: TextStyle(color: Colors.black)),
+                const TextSpan(text: ' tarif : '),
+                TextSpan(
+                    text: site.tarif.toString(),
+                    style: const TextStyle(
+                        fontWeight: FontWeight.bold, color: Colors.blue)),
+              ])),
         ],
+      ),
+      trailing: Checkbox(
+        value: true,
+        onChanged: (e) => {},
+        activeColor: Colors.blue,
+        checkColor: Colors.white,
       ),
     );
   }
